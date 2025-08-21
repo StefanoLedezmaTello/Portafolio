75e17fefe2b0acf2e712bb525ee6ea5b75fc4e65
StefanoLedezmaTello <s.ledezma.tello@gmail.com>
2025-08-20 23:36:29 -0400

    Cambio de informacion github, se agrega link con los datos correspondientes

--- src/components/proyectos/proyectos.jsx
+++ src/components/proyectos/proyectos.jsx
@@ -4,16 +4,16 @@ const proyectosData = [
   {
     id: 1,
     titulo: "Proyecto CV Personal",
-    imagen: "https://via.placeholder.com/300x180?text=Proyecto+CV",
+    // imagen: "https://via.placeholder.com/300x180?text=Proyecto+CV",
     descripcion:
       "Desarrollo de mi currículum digital con React, mostrando experiencia y habilidades.",
     tecnologias: ["React", "CSS", "JavaScript"],
-    github: "https://github.com/tuusuario/proyecto-cv",
+    github: "https://github.com/StefanoLedezmaTello/portafolio",
   },
   {
     id: 2,
     titulo: "APIs de Gestión de Menestra",
-    imagen: "https://via.placeholder.com/300x180?text=APIs+Menestra",
+    // imagen: "https://via.placeholder.com/300x180?text=APIs+Menestra",
     descripcion:
       "APIs para manejar información de compras, ventas y saldos de productos de una menestra.",
     tecnologias: [".NET", "SQL Server", "REST API"],
@@ -22,8 +22,8 @@ const proyectosData = [
   {
     id: 3,
     titulo: "Sistema de Ventas Minimarket",
-    imagen:
-      "https://via.placeholder.com/300x180?text=Sistema+Ventas+Minimarket",
+    // imagen:
+    //   "https://via.placeholder.com/300x180?text=Sistema+Ventas+Minimarket",
     descripcion:
       "Sistema de ventas con control de inventario, desarrollado con .NET, Vue, React y Bootstrap.",
     tecnologias: [".NET", "Vue", "React", "Bootstrap"],
